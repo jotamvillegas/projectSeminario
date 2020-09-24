537e9bd9759288121d7b4dcc3d55189faa7496ec
jvillegas <jvillegas@sysone.com>
2020-09-24 20:31:59 -0300

    primer commit

---
+++ sleepingmotorhome/src/main/java/com/seminario/sleepingmotorhome/SleepingmotorhomeApplication.java
@@ -0,0 +1,14 @@
+package com.seminario.sleepingmotorhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepingmotorhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepingmotorhomeApplication.class, args);
+        //  comentary
+    }
+
+}
